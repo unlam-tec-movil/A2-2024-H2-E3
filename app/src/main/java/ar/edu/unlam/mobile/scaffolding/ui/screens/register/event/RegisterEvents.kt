aafc5c99ef26ff074470e3eb1c05927233d4e196
TolozaLeo <150087986+TolozaLeo@users.noreply.github.com>
2024-09-27 19:29:35 -0300

    Revert "Register Screen"

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/register/event/RegisterEvents.kt
+++
@@ -1,8 +0,0 @@
-package ar.edu.unlam.mobile.scaffolding.ui.screens.register.event
-
-sealed class RegisterEvents {
-    class UpdateUsername(val username: String): RegisterEvents()
-    class UpdatePassword(val password: String): RegisterEvents()
-    class UpdateConfirmPassword(val password: String): RegisterEvents()
-    data object RegisterUser: RegisterEvents()
-}
--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/register/state/RegisterState.kt
+++
@@ -1,7 +0,0 @@
-package ar.edu.unlam.mobile.scaffolding.ui.screens.register.state
-
-data class RegisterState(
-    val usernameTextField: String = "",
-    val passwordTextField: String = "",
-    val confirmPasswordTextField: String = ""
-)
