8f41c0bcbd1a84032884f9e2b392bb8ef3b326be
TolozaLeo <leo_eltolo@hotmail.com>
2024-11-10 17:09:30 -0300

    Log.d eliminado

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/network/AuthInterceptor.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/network/AuthInterceptor.kt
@@ -1,6 +1,6 @@
 package ar.edu.unlam.mobile.scaffolding.data.network
 
-import android.util.Log
+import ar.edu.unlam.mobile.scaffolding.data.local.TokenManager
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -16,8 +16,6 @@ class AuthInterceptor(
                     tokenManager.userToken?.let { addHeader("Authorization", "Bearer $it") }
                     addHeader("Application-Token", tokenManager.appToken)
                 }.build()
-        //TODO eliminar Log
-        Log.d("AuthInterceptor", "userToken: ${tokenManager.userToken}, appToken: ${tokenManager.appToken}")
         return chain.proceed(request)
     }
 }
